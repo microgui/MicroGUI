f94e5d048661c01fc3e6794e188f60157093a37f
Carl Classon <carl.classon@hotmail.com>
2022-03-29 15:23:15 +0200

    Popup-dialog on JSON-button!
    Now when you press the 'Json-button' you get an option whether you would lika to copy the JSON directly to your clipboard or if you want to save it as a file.

--- ui/src/components/tools/Toolbar.js
+++ ui/src/components/tools/Toolbar.js
@@ -1,14 +1,21 @@
 import { useEditor } from '@craftjs/core'
-import { Button as MaterialButton, IconButton, Tooltip } from '@mui/material'
+import {
+    Button as MaterialButton,
+    IconButton,
+    Tooltip,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    Grid
+} from '@mui/material'
 
 import UndoIcon from '@mui/icons-material/Undo'
 import RedoIcon from '@mui/icons-material/Redo'
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline'
 import SaveAltIcon from '@mui/icons-material/SaveAlt'
-
 import { Link } from 'react-router-dom'
-
 import { useState } from 'react'
+import { saveAs } from 'file-saver'
 
 /**
  * Creates a toolbar for various tools related to the 
@@ -19,6 +26,19 @@ export const Toolbar = () => {
     const { actions, enabled, query } = useEditor((state) => ({
         enabled: state.options.enabled
     }))
+
+    const [open, setOpen] = useState(false)
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const saveFile = (data) => {
+        const file = new File([data], "test.json", {type:'text/json;charset=utf-8'})
+        saveAs(file)
+    }
 
     return (
         <div className='toolbar'>
@@ -89,27 +109,59 @@ export const Toolbar = () => {
                         Simulate
                     </MaterialButton>
                 </Link>
-                <MaterialButton
-                    size='small'
-                    variant='contained'
-                    color='success'
-                    /* 'query.serialize' serializes the state of the canvas
-                     into a JSON string. This can be used to recreate the
-                     state of the canvas. */
-                    onClick={() => {
-                        console.log(query.serialize())
-                    }}
-                    style={{
-                        margin: '5px', maxWidth: '70px',
-                        maxHeight: '35px', minWidth: '80px',
-                        minHeight: '25px'
-                    }
-                    }
-                    disableElevation
-                >
-                    <SaveAltIcon style={{ padding: '2px' }} />
-                    JSON
-                </MaterialButton>
+                
+                    <MaterialButton
+                        size='small'
+                        variant='contained'
+                        color='success'
+                        /* 'query.serialize' serializes the state of the canvas
+                         into a JSON string. This can be used to recreate the
+                         state of the canvas. */
+                        onClick={() => {
+                            console.log(query.serialize())
+                            handleOpen()
+                        }}
+                        style={{
+                            margin: '5px', maxWidth: '70px',
+                            maxHeight: '35px', minWidth: '80px',
+                            minHeight: '25px'
+                        }
+                        }
+                        disableElevation
+                    >
+                        <SaveAltIcon style={{ padding: '2px' }} />
+                        JSON
+                    </MaterialButton>
+                    <Dialog open={open} onClose={handleClose}>
+                        <DialogTitle>
+                            Hey you!
+                        </DialogTitle>
+                        <DialogContent>
+                            <Grid container>
+                                <Grid item xs={6}>
+                                    <h3>Copy to clipboard</h3>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <h3>Save as .JSON file</h3>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <MaterialButton 
+                                        variant='outlined' 
+                                        onClick={() => {navigator.clipboard.writeText(query.serialize())}}
+                                    > Copy
+                                    </MaterialButton>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <MaterialButton 
+                                        variant='outlined'
+                                        onClick={() => {saveFile(query.serialize())}}
+                                    >Save
+                                    </MaterialButton>
+                                </Grid>  
+                            </Grid>
+                        </DialogContent>
+                    </Dialog>
+                
             </div>
         </div>
     )
