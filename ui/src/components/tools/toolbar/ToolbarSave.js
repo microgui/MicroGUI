1ff402b79d2f5f1b1802165b893066140c7badb7
Linus Johansson <linus.r.johansson@gmail.com>
2022-07-21 11:59:38 +0200

    C-format is actually just a general String format, changed labels

--- ui/src/components/tools/toolbar/ToolbarSave.js
+++ ui/src/components/tools/toolbar/ToolbarSave.js
@@ -25,7 +25,7 @@ export const ToolbarSave = () => {
     const [openOuter, setOpenOuter] = useState(false)
     const [openInner, setOpenInner] = useState(false)
     const [copied, setCopied] = useState(false)
-    const [copiedC, setCopiedC] = useState(false)
+    const [copiedString, setcopiedString] = useState(false)
     const [formText, setFormText] = useState('')
     const [error, setError] = useState(false)
 
@@ -50,7 +50,7 @@ export const ToolbarSave = () => {
         setOpenOuter(false)
         setOpenInner(false)
         setCopied(false)
-        setCopiedC(false)
+        setcopiedString(false)
         setError(false)
         setFormText('')
     }
@@ -92,7 +92,7 @@ export const ToolbarSave = () => {
                         </Grid>
                         <Grid item xs={4}>
                             <h4>Copy to clipboard</h4>
-                            <h4>(C-format)</h4>
+                            <h4>(String-format)</h4>
                         </Grid>
                         <Grid item xs={4}>
                             <h4>Save as .json file</h4>
@@ -112,13 +112,13 @@ export const ToolbarSave = () => {
                         <Grid item xs={4}>
                             <MaterialButton
                                 variant='outlined'
-                                disabled={copiedC}
+                                disabled={copiedString}
                                 // copy the JSON data to the clipboard using craft.js functionality
                                 onClick={() => {
                                     navigator.clipboard.writeText(query.serialize().replaceAll('\\n', '').replaceAll('"', '\\"').replaceAll('    ', '').replaceAll('<div>', '\\n').replaceAll('</div>', ''))
-                                    setCopiedC(true)
+                                    setcopiedString(true)
                                 }}
-                            > {copiedC ? 'Copied' : 'Copy'}
+                            > {copiedString ? 'Copied' : 'Copy'}
                             </MaterialButton>
                         </Grid>
                         <Grid item xs={4}>
